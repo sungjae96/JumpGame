b30e749e5cafad29bdb0d24a3b07cdab8b542836
Hwang <suungjae9612@naver.com>
2020-04-17 14:05:41 +0900

    start

---
+++ UnitySample/Assets/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject wallPrefab;
+    public float interval;
+    public float rangeX = 5.0f;
+    public float rangeY = 3.0f;
+
+    IEnumerator Start()
+    { // yield을 사용하기 위해 IEnumerator type으로 return
+        while (true)
+        {
+            transform.position = new Vector3(Random.Range(-rangeX, -1), Random.Range(0, rangeY), transform.position.z);
+            Instantiate(wallPrefab, transform.position, transform.rotation);
+
+            yield return new WaitForSeconds(interval);
+        }
+    }    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
